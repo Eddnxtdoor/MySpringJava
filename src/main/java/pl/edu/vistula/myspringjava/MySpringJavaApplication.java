1f7002f79076ae00abbed8c65c4a533de8630991
Eddnxtdoor <hilaryseanedd@gmail.com>
2025-05-21 19:00:48 +0200

    Initial commit

---
+++ src/main/java/pl/edu/vistula/myspringjava/MySpringJavaApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.myspringjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/vistula/myspringjava/MySpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.myspringjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
